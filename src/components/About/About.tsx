8ab2dbd89c33e23afcbbd544ff93da219f50ad9b
Ryan Bradley <ryanbradley315@gmail.com>
2023-04-29 10:29:38 -0400

    Update About section styling for tablet view.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -19,33 +19,37 @@ export default function About() {
                 Former <span>Navy Submariner</span> turned <span>Full Stack Developer</span>.
             </div>
 
-            <div className={styles.mainText}>
-                <p>
-                    With almost 10 years of experience as a Nuclear-trained Electronics Technician in the Navy, my focus has always been attention to detail and results-oriented production. As a developer I take the same approach, building websites that serve a purpose and meet every need.
-                </p>
+            <div className={styles.container}>
 
-                <p>
-                    I learned full stack development and web design through a series of professional certification courses, such as the Full Stack Engineer course with Codecademy and UX Design course with Google. I’m solidifying my skills by working towards a degree in Software Engineering through Arizona State University.
-                </p>
-            </div>
+                <div className={styles.mainText}>
+                    <p>
+                        With almost 10 years of experience as a Nuclear-trained Electronics Technician in the Navy, my focus has always been attention to detail and results-oriented production. As a developer I take the same approach, building websites that serve a purpose and meet every need.
+                    </p>
 
-            <div className={styles.iconGrid}>
-                <IconDiv>
-                    <h4>Languages:</h4>
-                    <Languages />
-                </IconDiv>
-                <IconDiv>
-                    <h4>Frameworks:</h4>
-                    <Frameworks />
-                </IconDiv>
-                <IconDiv>
-                    <h4>Services:</h4>
-                    <Services />
-                </IconDiv>
-                <IconDiv>
-                    <h4>Design:</h4>
-                    <Design />
-                </IconDiv>
+                    <p>
+                        I learned full stack development and web design through a series of professional certification courses, such as the Full Stack Engineer course with Codecademy and UX Design course with Google. I’m solidifying my skills by working towards a degree in Software Engineering through Arizona State University.
+                    </p>
+                </div>
+
+                <div className={styles.iconGrid}>
+                    <IconDiv>
+                        <h4>Languages:</h4>
+                        <Languages />
+                    </IconDiv>
+                    <IconDiv>
+                        <h4>Frameworks:</h4>
+                        <Frameworks />
+                    </IconDiv>
+                    <IconDiv>
+                        <h4>Services:</h4>
+                        <Services />
+                    </IconDiv>
+                    <IconDiv>
+                        <h4>Design:</h4>
+                        <Design />
+                    </IconDiv>
+                </div>
+
             </div>
 
         </Section>
