2e567bd6285a15637585089d96d0f8e88728559f
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-20 17:17:10 -0400

    Add NavLink effect to illuminate active section based on url hash.

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import styles from './NavLink.module.css'
 
 type NavLinkProps = {
@@ -9,12 +9,36 @@ type NavLinkProps = {
 }
 
 export default function NavLink({ href, external, onClick, children }: NavLinkProps) {
-    const target = external ? '_blank' : ''
+    const [isActive, setIsActive] = useState(() => {
+        return window.location.hash === href
+    })
 
+    const target = external ? '_blank' : ''
     const rel = external ? 'noreferrer noopener' : ''
 
+    useEffect(() => {
+        const listener = () => {
+            if (window.location.hash === href) {
+                setIsActive(true)
+            } else {
+                setIsActive(false)
+            }
+        }
+
+        window.addEventListener('hashchange', listener)
+
+        return () => window.removeEventListener('hashchange', listener)
+    })
+
     return (
-        <a href={href} className={styles.navLink} target={target} rel={rel} onClick={onClick}>
+        <a 
+            href={href} 
+            className={styles.navLink} 
+            target={target} 
+            rel={rel} 
+            onClick={onClick}
+            style={{ color: isActive ? 'var(--color-purple-pri)' : '' }}
+        >
             {children}
         </a>
     )
