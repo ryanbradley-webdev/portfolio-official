21d860ded6f651a3b98a5c7590396238902f30ed
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-20 20:10:09 -0400

    Add api call for form submission and begin UI interaction for form submission.

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useRef, useState } from 'react'
+import axios from 'axios'
 import styles from './Connect.module.css'
 
 export default function Connect() {
+    const [messageSending, setMessageSending] = useState(false)
+    const [messageSent, setMessageSent] = useState(false)
+    const [messageError, setMessageError] = useState(true)
+    const [buttonMessage, setButtonMessage] = useState('Submit')
+
+    const nameRef = useRef<HTMLInputElement>(null)
+    const emailRef = useRef<HTMLInputElement>(null)
+    const subjectRef = useRef<HTMLInputElement>(null)
+    const messageRef = useRef<HTMLTextAreaElement>(null)
+
     const LinkedIn = () => (
         <a href='https://www.linkedin.com/in/ryanbradley-webdev/' target='_blank' rel='noopener noreferrer' className={styles.link}>
             LinkedIn
@@ -15,7 +27,41 @@ export default function Connect() {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+
+        setMessageSending(true)
+
+        const data = {
+            name: nameRef?.current?.value,
+            email: emailRef?.current?.value,
+            subject: subjectRef?.current?.value,
+            message: messageRef?.current?.value
+        }
+
+        axios.post(import.meta.env.VITE_SENDGRID_API_URL, data)
+            .then(() => {
+                setMessageSending(false)
+                setMessageSent(true)
+            })
+            .catch(() => {
+                setMessageSending(false)
+                setMessageError(true)
+            })
     }
+
+    useEffect(() => {
+        if (messageSending) {
+            setButtonMessage('Sending message...')
+        }
+
+        if (messageSent) {
+            setButtonMessage('Message Sent!')
+        }
+
+        if (messageError) {
+            setButtonMessage('Something went wrong')
+        }
+
+    }, [messageSending, messageSent, messageError])
 
     return (
         <section id="connect">
@@ -45,25 +91,32 @@ export default function Connect() {
                 <form action="" onSubmit={handleSubmit} className={styles.form}>
                     <label htmlFor="name">
                         Name
-                        <input type="text" id="name" name="name" placeholder="John Doe" required />
+                        <input type="text" id="name" name="name" placeholder="John Doe" ref={nameRef} required />
                         <div className={styles.inputUnderline}></div>
                     </label>
                     <label htmlFor="email">
                         Email
-                        <input type="email" id="email" name="email" placeholder="johndoe@example.com" required />
+                        <input type="email" id="email" name="email" placeholder="johndoe@example.com" ref={emailRef} required />
                         <div className={styles.inputUnderline}></div>
                     </label>
                     <label htmlFor="subject">
                         Subject
-                        <input type="text" id="subject" name="subject" placeholder="Enter your subject here" required />
+                        <input type="text" id="subject" name="subject" placeholder="Enter your subject here" ref={subjectRef} required />
                         <div className={styles.inputUnderline}></div>
                     </label>
                     <label htmlFor="message">
                         Message
-                        <textarea name="message" id="message" cols={30} rows={10} placeholder="Enter your message here" required></textarea>
+                        <textarea name="message" id="message" cols={30} rows={10} placeholder="Enter your message here" ref={messageRef} required></textarea>
                     </label>
-                    <button type="submit">
-                        Submit
+                    <button type="submit" className={styles.formBtn}>
+                        <span>{buttonMessage}</span>
+                        {
+                            (messageSending || messageSent || messageError)
+                            
+                            && 
+                            
+                            <div></div>
+                        }
                     </button>
                 </form>
 
